f77e2f0afd5639c7755f06fb8bc1cf0bbfd1d901
michaelgetachew-abebe <mikygetyos@gmail.com>
2023-09-16 18:24:28 +0300

    Feat(add): record adding form

--- crm/crmApp/forms.py
+++ crm/crmApp/forms.py
@@ -29,3 +29,13 @@ class SignUpForm(UserCreationForm):
             self.fields['password2'].label = ''
             self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
           
+
+class AddRecordForm(UserCreationForm):
+    first_name = forms.CharField(label="", max_length=50, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
+    last_name = forms.CharField(label="", max_length=50, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
+    email = forms.EmailField(label="", max_length=70,widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email'}))
+    phone = forms.CharField(label="", max_length=15, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': '+'}))
+    address = forms.CharField(label="", max_length=40, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': ''}))
+    city = forms.CharField(label="", max_length=50, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': ''}))
+    state = forms.CharField(label="", max_length=30, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': ''}))
+    zipcode = forms.CharField(label="", max_length=20, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': ''}))
